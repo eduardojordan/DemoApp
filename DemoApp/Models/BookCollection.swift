d24ed6d72f8cac9f8857057c65629e4288e5cc9d
MAC <>
2019-10-28 12:30:16 +0100

    Added bookCollection

---
+++ DemoApp/Models/BookCollection.swift
@@ -0,0 +1,16 @@
+//
+//  BookCollection.swift
+//  DemoApp
+//
+//  Created by MAC on 28/10/2019.
+//  Copyright © 2019 EdJordan. All rights reserved.
+//
+
+import Foundation
+
+
+struct BookCollection {
+    let kind : String
+    let totalItems: Int
+    let item:[Book]?
+}
