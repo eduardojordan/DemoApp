8a1c603c7c050e9dbf0a2dc11fcf00b768c74e94
MAC <>
2019-10-28 13:39:51 +0100

    Manually decode Book , only bookId and title

---
+++ DemoApp/Controllers/PrincipalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrincipalViewController.swift
+//  DemoApp
+//
+//  Created by MAC on 28/10/2019.
+//  Copyright © 2019 EdJordan. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DemoApp/Models/Book.swift
+++ DemoApp/Models/Book.swift
@@ -15,7 +15,45 @@ struct Book {
     let publishedDate: Date?
     let description: String?
     let coverURL: URL?
-    let ratings: Float?
+    let rating: Float?
     let numberOfReviews: Int?
     let price: Float?
+}
+
+extension Book: Decodable {
+  
+    enum CodingKeys: String, CodingKey {
+        case bookId = "id"
+        case volumeInfo
+        case title
+        case authors
+        case publishedDate
+        case description
+        case imageLinks
+        case coverURL = "thumbnail"
+        case rating = "averageRating"
+        case numberOfReview = "ratingsCount"
+        case saleInfo
+        case listPrice
+        case price = "amount"
+    }
+    
+    init(from decoder:Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        bookId = try container.decode(String.self, forKey: .bookId)
+        
+        let volumeInfo = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .volumeInfo)
+        title = try volumeInfo.decode(String.self, forKey: .title)
+       
+        //TODO
+        authors = nil
+        publishedDate = nil
+        description = nil
+        coverURL = nil
+        rating = nil
+        numberOfReviews = nil
+        price = nil
+    }
+    
 }
